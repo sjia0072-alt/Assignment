e6d5cc3a63e1b76fd2dd40c8ce59304fd48e3903
Shuhong Jiang <sjia0072@student.monash.edu>
2025-08-27 01:56:37 +1000

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import LoginForm from './components/LoginForm.vue'
+const loggedInUser = localStorage.getItem("user");
+function logout() {
+  localStorage.removeItem("user")
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div v-if="loggedInUser">
+    <div>
+      You are currently logged in as {{ loggedInUser }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <button @click="logout">Logout</button>
+  </div>
+  <div v-else>
+    <LoginForm />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
