3647c8cba687d2d670a375251e1ea7f8a68b9319
Shuhong Jiang <sjia0072@student.monash.edu>
2025-08-27 22:35:32 +1000

    Login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -8,7 +8,32 @@
             Sign Up/Login
           </div>
           <div class="card-body">
-            form
+            <form @submit.prevent="handleSubmit">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  placeholder="name@example.com"
+                  required
+                  v-model="email"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  required
+                  v-model="password"
+                />
+              </div>
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary">Login</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
