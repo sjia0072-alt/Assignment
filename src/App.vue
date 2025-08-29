e3e24ad54273c22df041a9c335350863716c5ad2
Shuhong Jiang <sjia0072@student.monash.edu>
2025-08-29 12:48:48 +1000

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <script setup>
+import { ref } from 'vue'
 import LoginForm from './components/LoginForm.vue'
-const loggedInUser = localStorage.getItem("user");
+const loggedInUser = ref(null)
+try {
+  loggedInUser.value = JSON.parse(localStorage.getItem("user"));
+} catch (e) {
+  // remove mal-formed data
+  localStorage.removeItem("user")
+}
 function logout() {
   localStorage.removeItem("user")
+  loggedInUser.value = null
+}
+
+function login(user) {
+  loggedInUser.value = user
 }
 </script>
 
 <template>
-  <div v-if="loggedInUser">
-    <div>
-      You are currently logged in as {{ loggedInUser }}
+  <main class='container mt-5'>
+    <div v-if="loggedInUser" class="row justify-content-center">
+      <div class="card shadow-lg col-lg-4 col-md-6">
+        <div class="card-body text-center">
+          <p class="card-text fs-5 mb-3">
+            Welcome back, <strong>{{ loggedInUser.name }}</strong>!
+          </p>
+          <ul class="list-group list-group-flush mb-4">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              <span>Email:</span>
+              <span class="fw-bold">{{ loggedInUser.email }}</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              <span>Role:</span>
+              <span class="badge bg-info text-dark fw-bold">{{ loggedInUser.role }}</span>
+            </li>
+          </ul>
+          <button class="btn btn-danger" @click="logout">
+            Log out
+          </button>
+        </div>
+      </div>
     </div>
-    <button @click="logout">Logout</button>
-  </div>
-  <div v-else>
-    <LoginForm />
-  </div>
+    <div v-else>
+      <LoginForm @login-success="login" />
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -11,24 +11,15 @@
             <form @submit.prevent="handleSubmit">
               <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="email"
-                  placeholder="name@example.com"
-                  required
-                  v-model="email"
-                />
+                <input type="email" class="form-control" id="email" placeholder="name@example.com" required
+                  v-model="email" />
               </div>
               <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="password"
-                  required
-                  v-model="password"
-                />
+                <input type="password" class="form-control" id="password" required v-model="password" />
+              </div>
+              <div v-if="errorMsg" class="mb-3 text-danger">
+                {{ errorMsg }}
               </div>
               <div class="d-grid gap-2">
                 <button type="submit" class="btn btn-primary">Login</button>
@@ -42,6 +33,32 @@
 </template>
 
 <script setup>
+
+import { ref } from 'vue';
+import users from '@/data/users.json';
+
+const email = ref('');
+const password = ref('');
+const errorMsg = ref('');
+
+const emit = defineEmits(['login-success']);
+
+function handleSubmit() {
+  for (const user of users) {
+    if (user.email == email.value && user.password == password.value) {
+      const loggedInUser = {
+        name: user.name,
+        email: user.email,
+        role: user.role
+      };
+      localStorage['user'] = JSON.stringify(loggedInUser);
+      emit('login-success', loggedInUser)
+      errorMsg.value = '';
+      return;
+    }
+  }
+  errorMsg.value = "Password mismatch"
+}
 </script>
 
 <style scoped></style>
